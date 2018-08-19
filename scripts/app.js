4264b3cb599157dc894acb905e67f28e41fbe279
neo-the-1 <41747132+neo-the-1@users.noreply.github.com>
2018-08-20 01:24:22 +0530

    Add files via upload

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,10 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+function hit() {
+  var key = 'hit';
+  if (sessionStorage.getItem(key) === true)
+    return;
+  $.post("https://neo-the-1.cfapps.io/hits", (data) => sessionStorage.setItem(key, true));
+}
