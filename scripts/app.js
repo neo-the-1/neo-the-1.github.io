a6959c8fd64743596a004efca7ed0120a46a8f74
Saurabh Shakya <shakya.saurabh@gmail.com>
2020-09-14 23:20:15 +0530

    update: social media links and image

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,3 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
-
-function hit() {
-  var key = 'hit';
-  if (sessionStorage.getItem(key) == "true")
-    return;
-  $.post("https://neo-the-1.cfapps.io/hits", (data) => sessionStorage.setItem(key, true));
-}
